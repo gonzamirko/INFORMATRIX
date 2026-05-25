d861d8ac362d19373bd2893d806ee84102713499
Gonzalo Greatti <gonza_lauty@outlook.com>
2026-05-25 19:02:09 -0300

    agrego una base del puzzle uno, ya se pueden agarrar los componentes de la pc

---
+++ Assets/Scripts/Puzzle/ClickableItem.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ClickableItem : MonoBehaviour
+{
+    [SerializeField] private string itemId;
+    [SerializeField] private string displayName;
+
+    [SerializeField] private float interactionDistance = 1.5f;
+
+    private bool wasCollected;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (wasCollected)
+        {
+            return;
+        }
+
+        float distance = Vector2.Distance(
+            transform.position,
+            player.transform.position
+        );
+
+        if (distance <= interactionDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                wasCollected = true;
+
+                InventoryManager.Instance.CollectItem(itemId);
+
+                Debug.Log("Recogiste: " + displayName);
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
