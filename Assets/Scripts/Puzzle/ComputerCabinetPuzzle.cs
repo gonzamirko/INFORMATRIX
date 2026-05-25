afb7d3062009e51331eb782e567e5feabce8196d
Gonzalo Greatti <gonza_lauty@outlook.com>
2026-05-25 20:19:36 -0300

    agrego el gabinete y el puzzle 1 queda funcional

---
+++ Assets/Scripts/Puzzle/ComputerCabinetPuzzle.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerCabinetPuzzle : MonoBehaviour
+{
+    [SerializeField] private float interactionDistance = 2f;
+
+    private bool isCompleted;
+
+    private GameObject player;
+
+    private readonly List<string> requiredItems = new()
+    {
+        "motherboard",
+        "cpu",
+        "ram",
+        "ssd",
+        "psu",
+        "gpu"
+    };
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (isCompleted)
+        {
+            return;
+        }
+
+        float distance = Vector2.Distance(
+            transform.position,
+            player.transform.position
+        );
+
+        if (distance <= interactionDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                bool hasAllItems = true;
+
+                foreach (string itemId in requiredItems)
+                {
+                    if (!InventoryManager.Instance.HasItem(itemId))
+                    {
+                        Debug.Log("Falta: " + itemId);
+                        hasAllItems = false;
+                        
+                    }
+                }
+
+                if (!hasAllItems)
+                {
+                    Debug.Log("Todavía faltan componentes.");
+                    return;
+                }
+
+                CompletePuzzle();
+            }
+        }
+    }
+
+    private void CompletePuzzle()
+    {
+        isCompleted = true;
+
+        Debug.Log("¡Computadora armada correctamente!");
+    }
+}
