a86d0bc051e7f6155eb610e81af8ade9fb217a16
ricartes123 <pedro-le2003@hotmail.com>
2026-06-01 20:20:04 -0300

    feat: integrate puzzle one assets obstacles and computer state

--- Assets/Scripts/Puzzle/PlayerMovement.cs
+++ Assets/Scripts/Puzzle/PlayerMovement.cs
@@ -7,6 +7,13 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5f;
 
+    [Header("Movement Bounds")]
+    [SerializeField] private Vector2 minBounds;
+    [SerializeField] private Vector2 maxBounds;
+
+    [Header("Collision")]
+    [SerializeField] private LayerMask obstacleLayer;
+
     private void Start()
     {
         targetPosition = transform.position;
@@ -16,13 +23,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(
+                Input.mousePosition
+            );
 
             mousePosition.z = 0;
 
-            targetPosition = mousePosition;
+            Vector3 clampedPosition = new(
+                Mathf.Clamp(mousePosition.x, minBounds.x, maxBounds.x),
+                Mathf.Clamp(mousePosition.y, minBounds.y, maxBounds.y),
+                0
+            );
 
-            isMoving = true;
+            RaycastHit2D obstacle = Physics2D.Linecast(
+                transform.position,
+                clampedPosition,
+                obstacleLayer
+            );
+
+            if (obstacle.collider == null)
+            {
+                targetPosition = clampedPosition;
+                isMoving = true;
+            }
         }
 
         if (isMoving)
